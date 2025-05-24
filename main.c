ad24875e2fe2e942da75d03ca88eb9b9878e0b89
Camerupt65 <evertonmarlonberlezi@gmail.com>
2025-05-23 21:41:51 -0300

    Update and rename código to main.c

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "constantes.h"
+#include "menus.h"
+#include "cliente.h"
+#include "produto.h"
+
+
+void menu();
+
+int main() {
+    menu();
+
+    return 0;
+}
+
+void menu() {
+    int opcao;
+    do {
+        mostraOpcoes();
+        scanf("%d", &opcao);
+        
+        switch(opcao) {
+            case 1: cadastrarCliente(); break;
+            case 2: insereProduto(); break;
+            case 3: listarClientes(); break;
+            case 4: menuEditarCliente(); break;
+            case 5: editarProdutos(); break;
+            case 0: printf("Encerrando...\n"); break;
+            default: printf("Opcao invalida.\n");
+        }
+    } while (opcao != 0);
+}
