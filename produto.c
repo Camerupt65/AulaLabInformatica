680e8509d72f8667971794241d908a7fd0c2081f
Camerupt65 <evertonmarlonberlezi@gmail.com>
2025-05-30 17:03:28 -0300

    Update produto.c add id

--- produto.c
+++ produto.c
@@ -4,6 +4,8 @@
 
 Produto produtosStruct[QTD_PRODUTOS];
 int qtdProdutosCadastrados = 0;
+int ultimoIdProduto = 0;
+
 
 void insereProduto() {
     if (qtdProdutosCadastrados >= QTD_PRODUTOS) {
@@ -12,9 +14,36 @@ void insereProduto() {
     }
     printf("Nome do produto: ");
     scanf(" %s", produtosStruct[qtdProdutosCadastrados].nome);
+    
+    ultimoIdProduto++;
+    produtosStruct[qtdProdutosCadastrados].id = ultimoIdProduto;
+    
     qtdProdutosCadastrados++;
 }
+
+void listarProdutos() {
+    if (qtdProdutosCadastrados == 0) {
+        printf("Nenhum produto cadastrado.\n");
+        return;
+    }
+    for (int i = 0; i < qtdProdutosCadastrados; i++) {
+        printf("id: %d | nome: %s\n", produtosStruct[i].id, produtosStruct[i].nome);
+    }
+}
+
 
 void editarProdutos() {
     printf("Funcao de editar produtos ainda nao implementada.\n");
 }
+
+
+
+
+
+
+
+
+
+
+
+
