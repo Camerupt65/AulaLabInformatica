373c10483c9ed0e86f50a178ea4fa48743c48f54
Camerupt65 <evertonmarlonberlezi@gmail.com>
2025-05-30 17:01:41 -0300

    Update cliente.c add id

--- cliente.c
+++ cliente.c
@@ -5,6 +5,8 @@
 
 Cliente clientesStruct[QTD_CLIENTES];
 int qtdClientesCadastrados = 0;
+int ultimoIdCliente = 0;
+
 
 // 1
 void cadastrarCliente() {
@@ -14,15 +16,24 @@ void cadastrarCliente() {
     }
     printf("Nome do cliente: ");
     scanf(" %s", clientesStruct[qtdClientesCadastrados].nome);
+    
+    ultimoIdCliente++;
+    clientesStruct[qtdClientesCadastrados].id = ultimoIdCliente;
+
     qtdClientesCadastrados++;
 }
 
 // 3
 void listarClientes() {
+    if (qtdClientesCadastrados == 0) {
+        printf("Nenhum cliente cadastrado.\n");
+        return;
+    }
     for (int i = 0; i < qtdClientesCadastrados; i++) {
-        printf("%d - %s\n", i + 1, clientesStruct[i].nome);
+        printf("id: %d | nome: %s\n", clientesStruct[i].id, clientesStruct[i].nome);
     }
 }
+
 
 // 4
 void menuEditarCliente() {
@@ -42,56 +53,71 @@ void menuEditarCliente() {
 
 // 4.1
 void buscarClienteMenu() {
-    char selecao[200];
-    int opcao, i = 0, encontrado = 0;
+    int id, index;
 
-    printf("Digite o nome do cliente a ser editado: ");
-    scanf(" %s", selecao);
+    printf("Digite o ID do cliente a ser editado: ");
+    scanf("%d", &id);
 
-    encontraIndexDeCliente(&encontrado, &i, selecao);
+    index = encontraIndexDeCliente(id);
 
-    if (!encontrado) {
-        printf("Cliente '%s' nao encontrado.\n", selecao);
+    if (index == -1) {
+        printf("Cliente com ID %d nao encontrado.\n", id);
     } else {
-        menuClienteSelecionado(selecao, i, encontrado);
+        menuClienteSelecionado(id, index);
     }
 }
-void encontraIndexDeCliente(int *encontrado, int *i, char selecao[200]) {
-    for (*i = 0; *i < qtdClientesCadastrados; (*i)++) {
-        if (strcmp(clientesStruct[*i].nome, selecao) == IGUAL) {
-            *encontrado = 1;
-            break;
+int encontraIndexDeCliente(int id) {
+    for (int i = 0; i < qtdClientesCadastrados; i++) {
+        if (clientesStruct[i].id == id) {
+            return i;
         }
     }
+    return -1;
 }
-void menuClienteSelecionado(char selecao[200], int i, int encontrado) {
+
+void menuClienteSelecionado(int id, int index) {
     int opcao;
     mostraOpcoesClienteSelecionado();
 
     scanf("%d", &opcao);
 
     switch (opcao) {
-        case 1: excluirCliente(selecao, i); break;
-        case 2: alterarCliente(selecao, i); break;
+        case 1: excluirCliente(id, index); break;
+        case 2: alterarCliente(id, index); break;
         case 0: break;
         default: printf("Opcao invalida.\n");
     }
 }
 
 // 4.1.1
-void excluirCliente(char clienteAExcluir[200], int index) {
+void excluirCliente(int id, int index) {
     for (int j = index; j < qtdClientesCadastrados - 1; j++) {
         clientesStruct[j] = clientesStruct[j + 1];
     }
     qtdClientesCadastrados--;
-    printf("Cliente '%s' excluido com sucesso.\n", clienteAExcluir);
+    printf("Cliente com ID %d excluído com sucesso.\n", id);
 }
 
+
 // 4.1.2
-void alterarCliente(char clienteAAlterar[200], int index) {
+void alterarCliente(int id, int index) {
     char novoNome[200];
     printf("Digite o novo nome: ");
     scanf(" %s", novoNome);
     strcpy(clientesStruct[index].nome, novoNome);
-    printf("Nome alterado com sucesso!\n");
+    printf("Cliente com ID %d alterado com sucesso!\n", id);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
