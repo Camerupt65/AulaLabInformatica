9ded766bd7f36cea27cb1aa9b19cdd46d1ddbf8e
Camerupt65 <evertonmarlonberlezi@gmail.com>
2025-06-07 11:03:41 -0300

    Update produto.c

--- produto.c
+++ produto.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "produto.h"
 #include "menus.h"
 
@@ -7,6 +8,7 @@ int qtdProdutosCadastrados = 0;
 int ultimoIdProduto = 0;
 
 
+// 4 insercao
 void insereProduto() {
     if (qtdProdutosCadastrados >= QTD_PRODUTOS) {
         printf("Limite de produtos atingido.\n");
@@ -21,20 +23,107 @@ void insereProduto() {
     qtdProdutosCadastrados++;
 }
 
+
+
+// 5 listagem
 void listarProdutos() {
     if (qtdProdutosCadastrados == 0) {
         printf("Nenhum produto cadastrado.\n");
         return;
     }
+    printf("\n--------------------------");
     for (int i = 0; i < qtdProdutosCadastrados; i++) {
-        printf("id: %d | nome: %s\n", produtosStruct[i].id, produtosStruct[i].nome);
+        printf("\nid: %d | nome: %s", produtosStruct[i].id, produtosStruct[i].nome);
+    }
+    printf("\n--------------------------\n");
+}
+
+
+
+// 6 menu de edicao
+void menuEditarProduto() {
+    int opcao;
+    do {
+        mostraOpcoesProdutos();
+        scanf("%d", &opcao);
+        switch(opcao) {
+            case 1: buscarProdutoMenu(); break;
+            case 2: listarProdutos(); break;
+            case 0: printf("Voltando...\n"); break;
+            default: printf("Opcao invalida.\n");
+        }
+    } while (opcao != 0);
+}
+
+// 6.1 busca
+void buscarProdutoMenu() {
+    int id, index;
+
+    printf("Digite o ID do produto a ser editado: ");
+    scanf("%d", &id);
+
+    index = encontraIndexDeProduto(id);
+
+    if (index == -1) {
+        printf("Produto com ID %d nao encontrado.\n", id);
+    } else {
+        menuProdutoSelecionado(id, index);
+    }
+}
+int encontraIndexDeProduto(int id) {
+    for (int i = 0; i < qtdProdutosCadastrados; i++) {
+        if (produtosStruct[i].id == id) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// 6.1 menu de edicao 2
+void menuProdutoSelecionado(int id, int index) {
+    int opcao;
+    mostraOpcoesProdutoSelecionado();
+
+    scanf("%d", &opcao);
+
+    switch (opcao) {
+        case 1: excluirProduto(id, index); break;
+        case 2: alterarProduto(id, index); break;
+        case 0: break;
+        default: printf("Opcao invalida.\n");
     }
 }
 
-
-void editarProdutos() {
-    printf("Funcao de editar produtos ainda nao implementada.\n");
+// 6.1.1 exclusao
+void excluirProduto(int id, int index) {
+    for (int j = index; j < qtdProdutosCadastrados - 1; j++) {
+        produtosStruct[j] = produtosStruct[j + 1];
+    }
+    qtdProdutosCadastrados--;
+    printf("Produto com ID %d excluído com sucesso.\n", id);
 }
+
+// 6.1.2 alteracao
+void alterarProduto(int id, int index) {
+    char novoNome[200];
+    printf("Digite o novo nome: ");
+    scanf(" %s", novoNome);
+    strcpy(produtosStruct[index].nome, novoNome);
+    printf("Produto com ID %d alterado com sucesso!\n", id);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
