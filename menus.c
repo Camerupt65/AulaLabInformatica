0028af0865b320a559d06479c809bc721d23a935
Camerupt65 <evertonmarlonberlezi@gmail.com>
2025-06-07 11:04:34 -0300

    Update menus.c

--- menus.c
+++ menus.c
@@ -2,19 +2,30 @@
 #include "menus.h"
 #include "constantes.h"
 
+// menu principal
 void mostraOpcoes() {
     printf(
+        "\n"
         "\nSelecione uma opcao:\n"
+        "\n"
         "0 - para sair\n"
+        "\n"
+        "------Clientes------\n"
         "1 - insere cliente\n"
-        "2 - insere produto\n"
-        "3 - listar clientes\n"
-        "4 - editar cliente\n"
-        "5 - editar produto\n"
-        "6 - listar produtos\n"
+        "2 - listar clientes\n"
+        "3 - editar cliente\n"
+        "\n"
+        "------Produtos------\n"
+        "4 - insere produto\n"
+        "5 - listar produtos\n"
+        "6 - editar produto\n"
+        "\n"
+        "-------Vendas-------\n"
+        "7 - gerar venda\n"
     );
 }
 
+// menus clientes
 void mostraOpcoesClientes() {
     printf(
         "\nSelecione uma opcao:\n"
@@ -23,7 +34,6 @@ void mostraOpcoesClientes() {
         "2 - listar clientes\n"
     );
 }
-
 void mostraOpcoesClienteSelecionado() {
     printf(
         "\nSelecione uma opcao:\n"
@@ -33,8 +43,23 @@ void mostraOpcoesClienteSelecionado() {
     );
 }
 
-
-
+// menus produtos
+void mostraOpcoesProdutos() {
+    printf(
+        "\nSelecione uma opcao:\n"
+        "0 - para voltar\n"
+        "1 - selecionar produto\n"
+        "2 - listar produtos\n"
+    );
+}
+void mostraOpcoesProdutoSelecionado() {
+    printf(
+        "\nSelecione uma opcao:\n"
+        "0 - voltar\n"
+        "1 - excluir\n"
+        "2 - alterar nome\n"
+    );
+}
 
 
 
